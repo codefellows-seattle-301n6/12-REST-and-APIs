3f898dc4b884a11e7ad51b38d48a036f977ec70e
Rosemary Tuttle <rosemary.brinton@gmail.com>
2016-12-20 19:44:57 -0800

    Completed handlebar compile function

--- starter-code/scripts/views/repoView.js
+++ starter-code/scripts/views/repoView.js
@@ -5,7 +5,8 @@
   /* TODO: Let's compile our new template!
        Save the result of invoking Handlebars in this 'repoCompiler' variable
        that we will pass to the append method below. */
-  var repoCompiler;  // Finish the Handlebars method here!
+  var repoCompiler =
+  Handlebars.compile($('#repo-template').html()); // Finish the Handlebars method here!
 
   repoView.renderRepos = function() {
     $('#about ul').empty().append(
