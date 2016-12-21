d102900999425a39f597c25c4a034b3607cd8ef7
Meagan Mason <meagananne@Meagans-MacBook-Pro.local>
2016-12-20 19:40:11 -0800

    Added Handlebars template and still working on requestRepos method

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -7,22 +7,16 @@
 // TODO: DONE: create a githubToken.js file that we can use to generate our headers
          // properly.
   reposObj.requestRepos = function(callback) {
-    /* TODO: How would you like to fetch your repos? Someone say AJAX?!
+    /* TODO: DONE: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
-      $.ajax({
-        url: 'https://api.github.com/user/repo?type=owner',
-        method: 'GET',
-        headers: {
-          Authorization: 'token ${githubToken}'
-        }
-      })
-      .then(
-        data => {
-          data.forEach(repo => $('results').append(`<p>${repo.name}</p>`))
-        },
-        err => {
-          console.error(err)
-        })
+    $.ajax({
+      url: 'https://api.github.com/user/repo?type=owner',
+      method: 'GET',
+      headers: {
+        Authorization: 'token ${githubToken}'
+      }
+    })
+      .then(callback);
   };
 
   reposObj.withTheAttribute = function(myAttr) {
