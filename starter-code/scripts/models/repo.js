639a0089dceeb16ca062f964b10ad26372656310
Meagan Mason <meagananne@Meagans-MacBook-Pro.local>
2016-12-20 19:33:30 -0800

    added scripts to html, created githubToken.js and working on requestRepos method

--- starter-code/scripts/models/repo.js
+++ starter-code/scripts/models/repo.js
@@ -4,11 +4,25 @@
   var reposObj = {};
 
   reposObj.allRepos = [];
-// TODO: create a githubToken.js file that we can use to generate our headers
+// TODO: DONE: create a githubToken.js file that we can use to generate our headers
          // properly.
   reposObj.requestRepos = function(callback) {
     /* TODO: How would you like to fetch your repos? Someone say AJAX?!
       Do not forget to call the callback! */
+      $.ajax({
+        url: 'https://api.github.com/user/repo?type=owner',
+        method: 'GET',
+        headers: {
+          Authorization: 'token ${githubToken}'
+        }
+      })
+      .then(
+        data => {
+          data.forEach(repo => $('results').append(`<p>${repo.name}</p>`))
+        },
+        err => {
+          console.error(err)
+        })
   };
 
   reposObj.withTheAttribute = function(myAttr) {
